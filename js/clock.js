17ba95fbe4a9973c516e004b0e5238bbcb5f97ea
antsao <antsao@calpoly.edu>
2015-02-03 22:21:47 -0800

    added alarms

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,28 @@
 function getTime() {
    var date = new Date();
    var hours = date.getHours();
-   var minutes = date.getMinutes();
-   var seconds = date.getSeconds();
-   document.getElementById('clock').innerHTML = hours + ":" + minutes + ":" + seconds;
+   var minutes = "" + date.getMinutes();
+   var seconds = "" + date.getSeconds();
+   var ampm;
+   if (hours >= 12) {
+      ampm = "pm";
+      if (hours != 12) {
+         hours = hours - 12;
+      }
+   }
+   else {
+      if (hours == 0) {
+         hours = 12;
+      }
+      ampm = "am";
+   }
+   if (minutes.length < 2) {
+      minutes = "0" + minutes;
+   }
+   if (seconds.length < 2) {
+      seconds = "0" + seconds;
+   }
+   document.getElementById('clock').innerHTML = hours + ":" + minutes + ":" + seconds + " " + ampm;
    setTimeout(function(){ getTime() }, 1000);
 }
 
@@ -30,4 +49,34 @@ function getTemp() {
             $("body").addClass("hot");
          }
       });
+}
+
+function showAlarmPopup() {
+   $("#mask").removeAttr("class");
+   $("#popup").removeAttr("class");
+}
+
+function hideAlarmPopup() {
+   $("#mask").attr("class", "hide");
+   $("#popup").attr("class", "hide");
+}
+
+function insertAlarm(hours, mins, ampm, alarmName) {
+   var newDiv = $("<div></div>").addClass("flexible");
+   var first = $("<div></div>").addClass("name");
+   first.html(alarmName);
+   var second = $("<div></div>").addClass("time");
+   second.html(hours + ":" + mins + " " + ampm);
+   newDiv.append(first);
+   newDiv.append(second);
+   $("#alarms").append(newDiv);
+}
+
+function addAlarm() {
+   var hours = $("#hours option:selected").text();
+   var mins = $("#mins option:selected").text();
+   var ampm = $("#ampm option:selected").text();
+   var alarmName = $("#alarmName").val();
+   insertAlarm(hours, mins, ampm, alarmName);
+   hideAlarmPopup();
 }
